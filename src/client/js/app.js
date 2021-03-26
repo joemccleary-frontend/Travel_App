1c70147dce80484e34283e2700b6991361c637b7
Joe <joemccleary0@gmail.com>
2021-03-26 11:35:46 +0000

    added results page, hidden input page

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -4,4 +4,21 @@
 
 //export other js files
 
-createResults()
+//APIs
+//Geonames (finds location)
+//Weatherbit (gets weather data)
+//Pixabay (finds image for location)
+//REST countries - takes location and returns info on country
+
+
+
+function createInput() {
+    document.getElementById("inputPage").classList.remove("invisible");
+    inputPage.scrollIntoView({behavior: "smooth"});
+
+}
+function createResults() {
+    document.getElementById("trip_deets").classList.remove("invisible2");
+    trip_deets.scrollIntoView({behavior: "smooth"});
+
+}
